612fe187196baaee0f5c43677a2eca3e9f9dfae6
Tuhan Sapumanage <tuhan.binuja@gmail.com>
2019-05-10 19:57:25 +0530

    Setting up the Storyboard structure & App Icon

---
+++ Sapumanage T. C. B. D.-COBSCComp171P-035/Controllers/LandingViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LandingViewController.swift
+//  Sapumanage T. C. B. D.-COBSCComp171P-035
+//
+//  Created by Tuhan Sapumanage on 5/10/19.
+//  Copyright © 2019 Tuhan Sapumanage. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+    override func viewDidAppear(_ animated: Bool) {
+        self.performSegue(withIdentifier: "TabsSegue", sender: nil)
+    }
+
+}
---
+++ Sapumanage T. C. B. D.-COBSCComp171P-035/Models/Student.swift
@@ -0,0 +1,13 @@
+//
+//  Student.swift
+//  Sapumanage T. C. B. D.-COBSCComp171P-035
+//
+//  Created by Tuhan Sapumanage on 5/10/19.
+//  Copyright © 2019 Tuhan Sapumanage. All rights reserved.
+//
+
+import Foundation
+
+class Student {
+    
+}
