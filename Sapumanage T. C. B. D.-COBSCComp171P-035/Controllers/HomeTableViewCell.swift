89a149f614a89d64a56844ee255abf0e3aa271dd
Tuhan Sapumanage <tuhan.binuja@gmail.com>
2019-05-12 01:18:30 +0530

    Design home page along with constraints and tab images after finding the most optimal image size. Tested for iPhone Xs, iPhone 8 and iPhone 5s (All devices running on iOS 11 and Above) #ToDo: Glyph image

---
+++ Sapumanage T. C. B. D.-COBSCComp171P-035/Controllers/HomeTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  HomeTableViewCell.swift
+//  Sapumanage T. C. B. D.-COBSCComp171P-035
+//
+//  Created by Tuhan Sapumanage on 5/12/19.
+//  Copyright © 2019 Tuhan Sapumanage. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UIView!
+    @IBOutlet weak var mobileLabel: UIView!
+    @IBOutlet weak var dpImageView: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Sapumanage T. C. B. D.-COBSCComp171P-035/Controllers/HomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomeViewController.swift
+//  Sapumanage T. C. B. D.-COBSCComp171P-035
+//
+//  Created by Tuhan Sapumanage on 5/12/19.
+//  Copyright © 2019 Tuhan Sapumanage. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HomeCell", for: indexPath) as! HomeTableViewCell
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
